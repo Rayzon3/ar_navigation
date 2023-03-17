a5a789117a1f07cf225514221829847ad4e5bc76
Deepika Bansal <deepikabansal@Deepikas-MacBook-Pro.local>
2023-03-17 09:28:03 +0530

    17-3-23

--- lib/routs.dart
+++ lib/routs.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_application_1/screens/complete_profile/complete_profile_screen.dart';
 import 'package:flutter_application_1/screens/forgot_password/forgot_password_screen.dart';
 import 'package:flutter_application_1/screens/login_success/login_success_screen.dart';
 import 'package:flutter_application_1/screens/sign_in/sign_in_screen.dart';
+import 'package:flutter_application_1/screens/sign_up/sign_up_screen.dart';
 import 'package:flutter_application_1/screens/splash/splash_screen.dart';
 
 
@@ -10,4 +12,6 @@ final Map<String, WidgetBuilder> routes = {
   SignInScreen.routeName: (context) => SignInScreen(),
   ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
   LoginSuccessScreen.routeName: (context) => LoginSuccessScreen(),
+  SignUpScreen.routeName: (context) => SignUpScreen(),
+  CompleteProfileScreen.routeName: (context) => CompleteProfileScreen(),
 };
